f4199b41cf2e002a122f6525297faec6b05aaa39
matthijs.delange@gmail.com <matthijs.delange@gmail.com@be3915f5-491d-e84f-4dcc-3bd8bdcdea7a>
2013-11-13 11:48:18 +0000

    Gewoon, om te proberen. Comments veranderd en een case-statement gemaakt

--- sputnik/sputnik.ino
+++ sputnik/sputnik.ino
@@ -1,34 +1,27 @@
 /*     -----------------------------------------------------------
- *     |  Arduino Experimentation Kit Example Code               |
- *     |  CIRC-03 .: Spin Motor Spin :. (Transistor and Motor)   |
+ *     |  Project Sputnik                                        |
+ *     |  Main controller                                        |
  *     -----------------------------------------------------------
  * 
- * The Arduinos pins are great for driving LEDs however if you hook 
- * up something that requires more power you will quickly break them.
- * To control bigger items we need the help of a transistor. 
- * Here we will use a transistor to control a small toy motor
- * 
- * http://tinyurl.com/d4wht7
+ * This is the master controller which supervises the driving
+ * All functions created here should be put in a library
  *
  */
  
 int echoPin =7; // Echo Pin
 int trigPin =8; // Trigger Pin
-int directionPort = 9;
-int motorPort = 10; // HIGH is forward, LOW is backward
-int ledPort = 14;
+int directionPort = 9; // HIGH is forward, LOW is backward
+int motorPort = 10;   // HIGH is on, LOW is off
+int ledPort = 14;     // Led for sonar treshold. 
 
 
 int onTime = 1000;  //the number of milliseconds for the motor to turn on for
 int offTime = 1000; //the number of milliseconds for the motor to turn off for
   
- int threshold = 100;
-  
-/*
- * setup() - this function runs once when you turn your Arduino on
- * We set the motors pin to be an output (turning the pin high (+5v) or low (ground) (-))
- * rather than an input (checking whether a pin is high or low)
- */
+int threshold = 100;  //In centimeters, the distance from objects where the car stops
+ 
+int dir = 1; // At the start we move forward 
+
 void setup()
 {
    Serial.begin (9600);
@@ -44,12 +37,18 @@ void setup()
  * loop() - this function will start after setup finishes and then repeat
  * we call a function called motorOnThenOff()
  */
-int dir = 1;
+
 void loop() {
-  if(dir == 1){
+switch dir {}
+case 1:
     forward();
-  }else{
+    break
+case 2:
     backward();
+    break
+default:
+    StopMoving()
+    break
   }
   int dist = readSonar();
   if(dist < threshold && dist != -1 ){
