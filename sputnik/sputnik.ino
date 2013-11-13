9cfa7b56252f88b95118014e16aa68562715fb10
toonen.meine <toonen.meine@be3915f5-491d-e84f-4dcc-3bd8bdcdea7a>
2013-11-13 12:29:03 +0000

    compiles again :)

--- sputnik/sputnik.ino
+++ sputnik/sputnik.ino
@@ -39,7 +39,7 @@ void setup()
  */
 
 void loop() {
-switch dir {
+switch (dir) {
 case 1:
     forward();
     break;
@@ -47,7 +47,7 @@ case 2:
     backward();
     break;
 default:
-    StopMoving();
+    stopMoving();
     break;
   }
   int dist = readSonar();
