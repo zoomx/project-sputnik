7f4ce7fb7d16f1546911e9319ae8b11b64de015e
toonen.meine <toonen.meine@be3915f5-491d-e84f-4dcc-3bd8bdcdea7a>
2013-11-27 20:07:42 +0000

    Turning implemented

--- sputnik/sputnik.ino
+++ sputnik/sputnik.ino
@@ -10,10 +10,14 @@
  
 int echoPin =7; // Echo Pin
 int trigPin =8; // Trigger Pin
+
 int directionPort = 9; // HIGH is forward, LOW is backward
 int motorPort = 10;   // HIGH is on, LOW is off
+
 int ledPort = 14;     // Led for sonar treshold. 
 
+int turnPort = 21;
+int turnPowerPort = 20;
 
 int onTime = 1000;  //the number of milliseconds for the motor to turn on for
 int offTime = 1000; //the number of milliseconds for the motor to turn off for
@@ -25,11 +29,42 @@ int dir = 1; // At the start we move forward
 void setup()
 {
    Serial.begin (9600);
-  pinMode(directionPort, OUTPUT); 
-  pinMode(motorPort, OUTPUT); 
-  pinMode(ledPort, OUTPUT); 
+    pinMode(directionPort, OUTPUT); 
+    pinMode(motorPort, OUTPUT); 
+    
+    
+    pinMode(turnPort, OUTPUT); 
+    pinMode(turnPowerPort, OUTPUT); 
+    
+    
+    pinMode(ledPort, OUTPUT); 
    pinMode(trigPin, OUTPUT);
    pinMode(echoPin, INPUT);
+}
+void dummyRun(){
+  delay(1000);
+  
+  Serial.println("Begin dummyrun");
+  forward();
+  delay(1000);
+  stopMoving();
+  delay(1000);
+  backward();
+  delay(1000);
+  stopMoving();
+  forward();
+  turn(1000, 0);
+  stopMoving();
+  backward();
+  delay(1000);
+  stopMoving();
+  forward();
+  turn(1000, 1);
+  stopMoving();
+  
+  Serial.println("End dummyrun");
+  
+  Serial.println("*************************************");
 }
 
 
@@ -39,35 +74,34 @@ void setup()
  */
 
 void loop() {
-switch (dir) {
-case 1:
-    forward();
-    break;
-case 2:
-    backward();
-    break;
-default:
-    stopMoving();
-    break;
-  }
-  int dist = readSonar();
-  if(dist < threshold && dist != -1 ){
-    digitalWrite(ledPort, HIGH);
-    if(dir != 2){
-      stopMoving();
+  dummyRun();
+  
+    /*
+  switch (dir) {
+      case 1:
+          forward();
+          break;
+      case 2:
+          backward();
+          break;
+      default:
+          stopMoving();
+          break;
     }
-    dir = 2;
-  }else{
-    digitalWrite(ledPort, LOW);
-    if(dir != 1){
-      stopMoving();
-    }
-    dir = 1;
-  }
-  /*forward();
-    backward();*/
-   
-   
+    int dist = readSonar();
+    if(dist < threshold && dist != -1 ){
+      digitalWrite(ledPort, HIGH);
+      if(dir != 2){
+        stopMoving();
+      }
+      dir = 2;
+    }else{
+      digitalWrite(ledPort, LOW);
+      if(dir != 1){
+        stopMoving();
+      }
+      dir = 1;
+    }*/
 }
 
 int readSonar(){
@@ -83,7 +117,7 @@ int readSonar(){
    duration -= 5;
    
   int distance = duration/76;
-  Serial.println(distance);
+  //Serial.println(distance);
   delay(50);
   if(distance ==0){
   distance = -1;
@@ -92,6 +126,7 @@ int readSonar(){
 }
 
 void forward(){
+  Serial.println("Vooruit");
   digitalWrite(motorPort, HIGH); // turns the motor On
   digitalWrite(directionPort, HIGH); // turns the motor On
   /*
@@ -102,6 +137,7 @@ void forward(){
 
 
 void backward(){
+  Serial.println("Achteruit");
   digitalWrite(motorPort, HIGH); // turns the motor On
   digitalWrite(directionPort, LOW); // turns the motor On
   /*
@@ -111,6 +147,17 @@ void backward(){
 }
 
 void stopMoving(){
+  Serial.println("Stop");
   digitalWrite(motorPort, LOW); // turns the motor On
   delay(1000);
 }
+
+void turn(int turnTime, int dir){ 
+  Serial.println("Turn " + dir);
+  digitalWrite(turnPort,255 * dir); // if dir=0, LOW, if 1, HIGH
+  digitalWrite(turnPowerPort,HIGH);
+  
+  delay(turnTime);
+  digitalWrite(turnPowerPort,LOW);
+}
+  
