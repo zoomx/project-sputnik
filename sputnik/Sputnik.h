6d9a36a9b2c0a78b751ca305f31a41c8f1466406
toonen.meine <toonen.meine@be3915f5-491d-e84f-4dcc-3bd8bdcdea7a>
2013-12-06 14:33:12 +0000

    Added library versions of sputnik

---
+++ sputnik/Sputnik.h
@@ -0,0 +1,37 @@
+#ifndef Sputnik_h
+#define Sputnik_h
+#include "Arduino.h"
+
+class Sputnik{
+	public:
+		Sputnik(int directionPin, int motorPin, int turnPin, int turnPowerPin);
+		Sputnik(int directionPin, int motorPin, int turnPin, int turnPowerPin,boolean verbose);
+		void forward();
+		void backward();
+		void turn(int turnTime, int direction);
+		void stopMoving();
+		void stopMoving(int stopTime);
+		
+	private:
+		boolean verbose = false;
+		int echoPin =7; // Echo Pin
+		int trigPin =8; // Trigger Pin
+
+		int directionPin = 9; // HIGH is forward, LOW is backward
+		int motorPin = 10;   // HIGH is on, LOW is off
+
+		int ledPin = 14;     // Led for sonar treshold. 
+
+		int turnPin = 21;
+		int turnPowerPin = 20;
+
+		int onTime = 1000;  //the number of milliseconds for the motor to turn on for
+		int offTime = 1000; //the number of milliseconds for the motor to turn off for
+		  
+		int threshold = 100;  //In centimeters, the distance from objects where the car stops
+		 
+		int dir = 1; // At the start we move forward */
+
+};
+
+#endif
